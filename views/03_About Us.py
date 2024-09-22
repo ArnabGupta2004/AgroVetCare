4c9796cf6d2c81e89e644ed8d53e311ade2bf40e
ArnabGupta2004 <115235503+ArnabGupta2004@users.noreply.github.com>
2024-09-22 22:41:43 +0530

    03_About Us.py

--- views/03_About Us.py
+++ views/03_About Us.py
@@ -3,12 +3,20 @@ import streamlit as st
 st.title("About")
 st.markdown("""
             #### About Dataset
-            This dataset is recreated using offline augmentation from the original dataset.The original dataset can be found on this github repo.
-            This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
-            A new directory containing 33 test images is created later for prediction purpose.
+            This dataset is recreated using offline augmentation from the original dataset.
+            This dataset consists of about 90K rgb images of healthy and diseased Crop leaves and Livestock(only cows due to unavailabilty of good quality dataset) which is categorized into 45 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
             #### Content
-            1. train (70295 images)
-            2. test (33 images)
-            3. validation (17572 images)
-
             """)
+tab1, tab2 = st.tabs(["Crops", "Livestocks"])
+with tab1:
+    st.markdown("""
+1. Train Set **(70295 Images)**  
+2. Valid Set **(33 Images)**  
+3. Validation Set **(17572 Images)**  
+    """)
+with tab2:
+    st.markdown("""
+1. Train Set **(567 Images)**  
+2. Valid Set **(81 Images)**  
+3. Validation Set **(186 Images)**  
+    """)
