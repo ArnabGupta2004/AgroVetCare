ce6133ec4694f212eaea9097abdd81107c5fc181
ArnabGupta2004 <115235503+ArnabGupta2004@users.noreply.github.com>
2024-09-22 12:26:49 +0530

    05_Vets.py

--- views/05_Vets.py
+++ views/05_Vets.py
@@ -46,7 +46,10 @@ map_height = 500  # Set a fixed height
 st.header("Map of Nearby Vets and Medicine Shops")
 folium_static(m, width=map_width, height=map_height)
 
-st.write("🔵➜ Your Location 🔴➜ Vets 🟢➜ Medicines")
+st.markdown("""🔵➜ Your Location  
+🔴➜ Vets  
+🟢➜ Medicines  
+""")
 
 # Button to trigger the pop-up
 if st.button("Add Your Store"):
