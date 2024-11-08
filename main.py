f0e5ad41bceebbc94213b214bbca2f82bba9b337
ArnabGupta2004 <115235503+ArnabGupta2004@users.noreply.github.com>
2024-11-08 20:59:32 +0530

    main.py

--- main.py
+++ main.py
@@ -32,12 +32,10 @@ app_mode.run()
 
 # Display logo and sidebar information
 st.logo("AgroVet Care_logo.png")
-st.sidebar.text("Made by Team Code&Conquer")
+
 
 # Feedback Section in Sidebar
-st.sidebar.markdown("---")  # Separator
-st.sidebar.subheader("We Value Your Feedback")
-feedback = st.sidebar.text_area("Please provide your feedback below:")
+feedback = st.sidebar.text_area()
 
 if st.sidebar.button("Submit Feedback"):
     if feedback:
@@ -45,3 +43,5 @@ if st.sidebar.button("Submit Feedback"):
         # Here you can process the feedback, e.g., store it in a database or send via email
     else:
         st.sidebar.warning("Please enter your feedback before submitting.")
+
+st.sidebar.text("Made by Team Code&Conquer")
