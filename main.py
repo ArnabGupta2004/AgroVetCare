6a524aec2554b46ef76a382834fd69572b14c1bd
ArnabGupta2004 <115235503+ArnabGupta2004@users.noreply.github.com>
2024-12-03 16:34:09 +0530

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import tensorflow as tf
 import numpy as np
 from streamlit_option_menu import option_menu
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # Define your pages
 home = st.Page(
     page="views/01_Home.py",
