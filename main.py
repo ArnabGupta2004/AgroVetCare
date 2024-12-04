6eca216f15ffa7044989364233f1fc9143334be2
ArnabGupta2004 <115235503+ArnabGupta2004@users.noreply.github.com>
2024-12-04 15:50:22 +0530

    main.py

--- main.py
+++ main.py
@@ -3,55 +3,57 @@ import tensorflow as tf
 import numpy as np
 from streamlit_option_menu import option_menu
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+st.set_page_config(
+    page_title="AgroVet Care",       # Set the page title
+    layout="centered",                   # Use wide layout
+    initial_sidebar_state="collapsed"  # Sidebar starts collapsed (closed)
+)
 
-# Define your pages
-home = st.Page(
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("styles\style.css")
+
+home=st.Page(
     page="views/01_Home.py",
     title="Home",
     default=True
 )
-dr = st.Page(
+dr=st.Page(
     page="views/02_Disease Recognition.py",
     title="Disease Recognition",
 )
-we = st.Page(
+we=st.Page(
     page="views/04_Weather.py",
     title="Weather Alerts",
 )
-about = st.Page(
+about=st.Page(
     page="views/03_About Us.py",
     title="About Us",
 )
-vet = st.Page(
+vet=st.Page(
     page="views/05_Vets.py",
     title="Nearby Vets",
 )
 
-# Navigation menu
-app_mode = st.navigation(pages=[home,about])
+app_mode= st.navigation(pages=[home,dr,we,vet,about])
 app_mode.run()
-
-# Display logo and sidebar information
 st.logo("AgroVet Care_logo.png")
 
+st.sidebar.subheader(":mailbox: Get In Touch With Us!")
+contact_form="""
+<form action="https://formsubmit.co/codeandconquer26@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your Name" required>
+     <input type="email" name="email" placeholder="Your Email" required>
+     <textarea name="message" placeholder="Give Feedback"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
 
-# Feedback Section in Sidebar
-st.sidebar.subheader("We Value Your Feedback")
-feedback = st.sidebar.text_area("Please provide your feedback below:")
-
-if st.sidebar.button("Submit Feedback"):
-    if feedback:
-        st.sidebar.success("Thank you for your feedback!")
-        # Here you can process the feedback, e.g., store it in a database or send via email
-    else:
-        st.sidebar.warning("Please enter your feedback before submitting.")
-
-st.sidebar.text("Made by Team Code&Conquer")
+st.sidebar.markdown(contact_form, unsafe_allow_html=True)
+st.sidebar.markdown("---")
+st.sidebar.text("Made by Team Code&Conquer_TMSL")
