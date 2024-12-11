c6d69df947df961acaf95eaa95a0bf78a5823100
ArnabGupta2004 <115235503+ArnabGupta2004@users.noreply.github.com>
2024-12-11 18:56:39 +0530

    Update 06_Education.py

--- views/06_Education.py
+++ views/06_Education.py
@@ -23,11 +23,8 @@ with colt:
 
 # Function to translate text
 def translate_text(text, lang='en'):
-    try:
-        translated = translator.translate(text, dest=lang)
-        return translated.text
-    except Exception as e:
-        return text  # Return original text if translation fails
+    return text  # Return the original text if translation fails
+
 
 # Page description
 st.markdown(translate_text("FarmHelp offers quick insights on disease identification, prevention, and cures to help farmers maintain healthy crops and livestock.", selected_language))
